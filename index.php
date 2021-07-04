1c38fc3a4aab85a6f1f44022efe91a3ee474d6c4
noelia <noeliarolli82@gmail.com>
2021-07-04 18:05:44 -0300

    actividad php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+echo "Nombre: Noelia";
+
+echo "<br>";
+
+echo "Apellido: Rolli";
+
+echo "<br>";
+
+echo "Edad: 39";
+
+echo "<br>";
+
+echo  "Hobbie: Viajar";
+
+echo "<br>";
+
+echo "Editor de código preferido: Visual Studio Code";
+
+echo "<br>";
+
+echo "Sistema operativo que utilizo: Linux";
+
+
+?>
